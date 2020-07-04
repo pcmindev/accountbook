f2312180de8c30f1b64a78f490f7491d565b1816
pcmindev <55571684+pcmindev@users.noreply.github.com>
2020-07-04 16:23:19 +0900

    필수항목 확인/생성 & flex2T 스타일 추가

--- main.js
+++ main.js
@@ -8,8 +8,41 @@ const app = express();
  * 앱 바탕 정보를 저장합니다.
  */
 const appInfo = {
-    port : 5000
+    port : 5000,
+    saveItemDirPath : './data/',
+    saveItemFileName : 'item.json',
+    saveItemExist : 0
 }
+
+// 주요 디렉토리 기본값인지 확인
+fs.stat(appInfo.saveItemDirPath, (err)=>{
+    if(err){
+        // 디렉토리 미존재 => 생성
+        if(err.code === 'ENOENT'){
+            console.log(`Create nessary directory: ${appInfo.saveItemDirPath}`);
+            fs.mkdirSync(appInfo.saveItemDirPath, (err)=>{if(err) throw err;});
+        }
+        else throw err;
+    }
+    // 디렉토리 존재
+    // 주요 파일이 기본값인지 확인
+    fs.stat(appInfo.saveItemDirPath + appInfo.saveItemFileName, (err)=>{
+        if(err){
+            // 주요 파일 미존재 => 생성
+            if(err.code === 'ENOENT'){
+                console.log(`Create Save file: ${appInfo.saveItemDirPath + appInfo.saveItemFileName}`);
+                fs.writeFile(appInfo.saveItemDirPath + appInfo.saveItemFileName, '', 'utf-8', (err)=>{if(err) throw err;});
+            }
+            else throw err;
+        }
+        // 주요파일 존재
+        else appInfo.saveItemExist = 1;
+    });
+});
+
+/**************************************************************************
+ * 앱 서버 서비스 영역                                                     *
+ **************************************************************************/
 
 /**
  * host/
@@ -35,7 +68,7 @@ app.post('/save', (req, res)=>{
  * 
  * 읽기 내용 보내는 주소
  */
-app.post('/read', (req, res)=>{
+app.get('/read', (req, res)=>{
 
 });
 
